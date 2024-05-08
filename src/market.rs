221cf91bbc4fd68daad38c7bc575a39b9770302c
WangShayne <1614565666@qq.com>
2024-05-08 16:17:43 +0800

    fix: Result Type

--- src/market.rs
+++ src/market.rs
@@ -75,7 +75,9 @@ impl Market {
     /// let prices = tokio_test::block_on(market.get_all_prices());
     /// assert!(prices.is_ok(), "{:?}", prices);
     /// ```
-    pub async fn get_all_prices(&self) -> Result<Prices> { self.client.get(API_V3_TICKER_PRICE, None).await }
+    pub async fn get_all_prices(&self) -> Result<Prices> {
+        self.client.get(API_V3_TICKER_PRICE, None).await
+    }
 
     /// Latest price for ONE symbol.
     /// # Examples
@@ -121,7 +123,9 @@ impl Market {
     /// let tickers = tokio_test::block_on(market.get_all_book_tickers());
     /// assert!(tickers.is_ok(), "{:?}", tickers);
     /// ```
-    pub async fn get_all_book_tickers(&self) -> Result<BookTickers> { self.client.get(API_V3_BOOK_TICKER, None).await }
+    pub async fn get_all_book_tickers(&self) -> Result<BookTickers> {
+        self.client.get(API_V3_BOOK_TICKER, None).await
+    }
 
     /// -> Best price/qty on the order book for ONE symbol
     /// # Examples
@@ -157,8 +161,9 @@ impl Market {
         self.client.get(API_V3_24H_TICKER, Some(&request)).await
     }
 
-    pub async fn get_24h_all_prices_stats(&self) -> Result<Prices> { self.client.get(API_V3_24H_TICKER, None).await }
-
+    pub async fn get_24h_all_prices_stats(&self) -> Result<PriceStats> {
+        self.client.get(API_V3_24H_TICKER, None).await
+    }
 
     /// Get aggregated historical trades.
     /// If you provide start_time, you also need to provide end_time.
